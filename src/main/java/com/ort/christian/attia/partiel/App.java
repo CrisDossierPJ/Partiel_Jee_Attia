534e345c03f3f5aea889a7400304955ca0b82aaf
CrisDossierPJ <christian_attia@hotmail.com>
2019-01-30 11:51:02 +0100

    first commit

---
+++ src/main/java/com/ort/christian/attia/partiel/App.java
@@ -0,0 +1,13 @@
+package com.ort.christian.attia.partiel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
